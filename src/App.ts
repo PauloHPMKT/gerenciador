d041dc5a46556d7ac7f3afac9a946c0062962173
Paulo Sergio <pauloserg861@gmail.com>
2024-06-25 19:30:58 -0300

    iniciando servidor

--- src/App.ts
+++ src/App.ts
@@ -1,1 +1,15 @@
-console.log('Ola mundo!')
+import express, { Application } from "express";
+
+export class App {
+  private app: Application;
+
+  constructor() {
+    this.app = express();
+  }
+
+  initServer(port: number) {
+    this.app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`)
+    })
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { App } from "./App";
+
+const app = new App();
+const port = 3000;
+
+app.initServer(port);
